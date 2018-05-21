276fba2611d279192588dff25a6f148c971791f7
Mike Chen <mike@mikechen.is>
2018-05-21 01:15:38 -0700

    Test feed

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// TODO:
+// An update for each match.
+// It'd be cool if it could be a little more flavorful with match previews.
+// Will the match determine who makes it to the playoffs?
+// Do we need off-day announcements?
+
+// 0 Match preview
+// 1 First match over. Summarize? Or break up?
+// 2 Second match over
+// 3 Third match over
---
+++ src/models/feed.ts
@@ -0,0 +1,8 @@
+export interface FeedItem {
+  uid: string;
+  updateDate: string;
+  titleText: string;
+  mainText: string;
+  redirectionUrl?: string;
+  streamUrl?: string;
+}
